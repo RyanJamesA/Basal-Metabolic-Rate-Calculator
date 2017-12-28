2b8b370020fdea99111bfe5483b42fbca84f6644
RyanJamesA <33883135+RyanJamesA@users.noreply.github.com>
2017-12-28 14:00:12 -0700

    Add files via upload

--- find_your_bmr.py
+++ find_your_bmr.py
@@ -1,95 +1,107 @@
-def get_bmr_male(weight, height, age):
-    bmr_cals_male = (10 * float(weight)) + (6.25 * float(height)) - (5 * float(age)) + 5
-    print("You are expending " + str(bmr_cals_male) + " calories just by existing!")
+import sys
 
-def get_bmr_female(weight, height, age):
-    bmr_cals_female = (10 * float(weight)) + (6.25 * float(height)) - (5 * float(age)) - 161
-    print("You are expending " + str(bmr_cals_female) + " calories just by existing!")
-    
+def main():
+    """Calls the other functions and then calls my_calculations()"""
+    print("\n\tWelcome to the BMR calculator!")
+    print("\tPress 'q' at any time to stop!")
+    active = True
+    while active:
+        units = get_unit()
+        sex = get_sex()
+        age = get_age()
+        height = get_height()
+        weight = get_weight()
+        my_calculations(units, sex, age, height, weight)
 
-bmr_questions = True
+def get_unit():
+    """Lets the user set his unit preference"""
+    print("\nDo you prefer Imperial or Metric units?")
+    imp_or_met_input = input("imp/met: ")
+    while True:
+        if imp_or_met_input == 'imp' or imp_or_met_input == 'met':
+            return imp_or_met_input
+        if imp_or_met_input == 'q':
+            sys.exit()
+        else:
+            print("Sorry, only 'met' and 'imp' works.")
+            get_unit()
 
-while bmr_questions:
-    print("For your basal metabolic rate, please enter your sex (male/female): ")
-    print("(press 'q' to exit) ")
-    sex = input("Sex: ").lower()
-    if sex == "male":
-        print("Now please enter your weight, height, and age: ")
-        print("For weight, please choose your preferred unit: ")
-        print("(press 'q' to exit) ")
-        lbs_or_kgs = input("Enter 'kg' or 'lbs' ").lower()
-        if lbs_or_kgs == 'kg':
-            total_weight_male = float(input("Weight: "))
-        if lbs_or_kgs == 'lbs':
-            weight_male = float(input("Weight: "))
-            total_weight_male = weight_male * 0.45359237
-        if lbs_or_kgs == 'q':
-            break
-        
-        
-        print("For height, please choose your preferred unit: ")
-        print("(press 'q' to exit)")
-        in_or_cm = input("Enter 'in' or 'cm' ").lower()
-        if in_or_cm == 'cm':
-            total_height_male = float(input("Height: "))
-        if in_or_cm == 'in':
-            height_male = float(input("Height: "))
-            total_height_male = height_male * 2.54
-        if in_or_cm == 'q':
-            break
-    
+def get_sex():
+    """Lets the user set his sex"""
+    print("\nWhat is your sex?")
+    get_sex_input = input("male/female: ")
+    while True:
+        if get_sex_input == 'male' or get_sex_input == 'female':
+            return get_sex
+        if get_sex_input == 'q':
+            sys.exit()
+        else:
+            print("Sorry, only 'male' and 'female' inputs work!")
+            get_sex()
 
-        print("For age, please enter your age in years (eg: 18) ")
-        print("(press 'q' to exit) ")
-        age_male = int(input("Age: "))
-        if age_male == 'q':
-            break
-        
-            
-        get_bmr_male(float(total_weight_male), float(total_height_male), age_male)
-    
+def get_age():
+    """Lets the user set his age"""
+    try:
+        print("\nHow old are you? ")
+        age_input = input("ex 18: ")
+        if age_input == 'q':
+            sys.exit()
+        else:
+            age_input = int(age_input)
+            return age_input
+    except ValueError:
+        print("Sorry, numbers only please!")
+        get_age()
 
-    
+def get_height():
+    """Lets the user set his height"""
+    try:
+        print("\nHow tall are you?")
+        height_input = input("ex 73: ")
+        if height_input == 'q':
+            sys.exit()
+        else:
+            return float(height_input)
+    except ValueError:
+        print("Sorry, numbers only please!")
+        get_height()
 
-    elif sex == "female":
-        print("Now please enter your weight, height, and age: ")
-        print("For weight, please choose your preferred unit: ")
-        print("(press 'q' to exit)")
-        lbs_or_kgs = input("Enter 'kg' or 'lbs' ").lower()
-        if lbs_or_kgs == 'kg':
-            total_weight_female = float(input("Weight: "))
-        if lbs_or_kgs == 'lbs':
-            weight_female = float(input("Weight: "))
-            total_weight_female = weight_female * 0.45359237
-        if lbs_or_kgs == 'q':
-            break
+def get_weight():
+    """Lets the user set his weight"""
+    try:
+        print("\nHow much do you weigh?")
+        weight_input = input("ex 202/70: ")
+        if weight_input == 'q':
+            sys.exit()
+        else:
+            return float(weight_input)
+    except ValueError:
+        print("Sorry, numbers only please!")
+        get_height()
 
-        print("For height, please choose your preferred unit: ")
-        print("(press 'q' to exit) ")
-        in_or_cm = input("Enter 'in' or 'cm' ").lower()
-        if in_or_cm == 'in':
-            height_female = float(input("Height: "))
-            total_height_female = height_female * 2.54
-        if in_or_cm == 'cm':
-            total_height_female = float(input("Height: "))
-        if in_or_cm == 'q':
-            break
+def my_calculations(units, sex, age, height, weight):
+    """Combines the measurements gathered from main() and does calculations"""
+    if units == 'imp':
+        user_height = height * 2.54
+        user_weight = weight * 0.45359237
+        bmr_cals = (user_weight * 10) + (user_height * 6.25) - (5 * age) 
+        + (5 if sex == 'male' else -161)
+        print(f"You are expending {bmr_cals} calories just by existing!")
+    if units == 'met':
+        bmr_cals = (weight * 10) + (height * 6.25) - (5 * age) 
+        + (5 if sex == 'male' else -161)
+        print(f"You are expending {bmr_cals} calories just by existing!")
+    start_again()
 
-        print("For age, please enter your age in years (eg: 18) ")
-        print("(press 'q' to exit) ")
-        age_female = int(input("Age: "))
-        if age_female == 'q':
-            break
+def start_again():
+    while True:
+        print("\nWould you like to do another one?")
+        user_response = input("y/n ")
+        if user_response == 'y':
+            main()
+        if user_response == 'n' or user_response == 'q':
+            sys.exit()
+        else:
+            print("Sorry, the only valid responses are y/n/q")
 
-        get_bmr_female(float(total_weight_female), float(total_height_female), age_female)
-
-        
-
-    elif sex == 'q':
-        break
-
-    again = input("Would you like to check another? ").lower()
-    if again == 'no':
-        bmr_questions = False
-
-
+main()
